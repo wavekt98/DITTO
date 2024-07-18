36636cff144d87a884ef29e3434ae450b0a001f7
이서빈 <bommy00@naver.com>
2024-07-18 11:07:34 +0900

    ✨[feat] #S11P12A106-27 #in-progress JwtFilter에서 resolveToken 메소드 추가

---
+++ back/src/main/java/com/ssafy/ditto/global/jwt/JwtFilter.java
@@ -0,0 +1,27 @@
+package com.ssafy.ditto.global.jwt;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+public class JwtFilter extends OncePerRequestFilter {
+    public final static String HEADER_NAME="Authorization";
+    private final JwtProvider jwtProvider;
+
+    private String resolveToken(HttpServletRequest request) {
+        String bearerToken = request.getHeader(HEADER_NAME);
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer")) {
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
+}
