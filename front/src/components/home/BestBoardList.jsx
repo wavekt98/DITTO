12e2d63097c22f868565fc95a9785cf3c58390c5
sehaim <seha3722@gmail.com>
2024-07-18 17:52:55 +0900

    :art:[Feat] #S11P12A106-55 #in-progress 홈 화면 컴포넌트 구성

---
+++ front/src/components/home/BestBoardList.jsx
@@ -0,0 +1,3 @@
+function BestBoardList() {}
+
+export default BestBoardList;
--- front/src/pages/HomePage.jsx
+++ front/src/pages/HomePage.jsx
@@ -1,77 +1,74 @@
-import { useState } from "react";
-import { useSelector, useDispatch } from "react-redux";
+// import { useState } from "react";
+// import { useSelector, useDispatch } from "react-redux";
 import { styled } from "styled-components";
 
-import { increment, decrement } from "../features/counter/counterSlice";
-import Button from "../components/common/Button";
-import RoundButton from "../components/common/RoundButton";
-import OutlineButton from "../components/common/OutlineButton";
-import Modal from "../components/common/Modal";
+// import { increment, decrement } from "../features/counter/counterSlice";
+import Banner from "../components/home/Banner";
+import BestBoardList from "../components/home/BestBoardList";
+import BestClassList from "../components/home/BestClassList";
+import NewClassList from "../components/home/NewClassList";
 
-const Div = styled.div`
-  background-color: var(--LIGHT);
+const HomePageContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
 `;
 
 function HomePage() {
-  const [modalOpen, setModalOpen] = useState(false);
-  const count = useSelector((state) => state.counter.value);
-  const dispatch = useDispatch();
-
-  const handleIncrement = () => {
-    dispatch(increment());
-  };
-
-  const handleDecrement = () => {
-    dispatch(decrement());
-  };
-
   return (
-    <div>
-      <button onClick={() => setModalOpen(true)}>모달 오픈</button>
-      {modalOpen && (
-        <Modal onClose={() => setModalOpen(false)}>
-          <Div>hello</Div>
-        </Modal>
-      )}
-      <h1>count: {count}</h1>
-      <Button label="증가" size="sm" onClick={handleIncrement} />
-      <Button label="증가" size="md" onClick={handleIncrement} />
-      <Button
-        label="감소"
-        color="default"
-        size="lg"
-        onClick={handleDecrement}
-      />
-      <Button
-        label="감소"
-        color="default"
-        size="xl"
-        onClick={handleDecrement}
-      />
-      <OutlineButton label="증가" size="sm" onClick={handleIncrement} />
-      <OutlineButton label="감소" color="default" onClick={handleDecrement} />
-      <OutlineButton
-        label="감소"
-        size="lg"
-        color="ACCENT1"
-        onClick={handleDecrement}
-      />
-      <OutlineButton
-        label="감소"
-        size="xl"
-        color="ACCENT1"
-        onClick={handleDecrement}
-      />
-      <RoundButton label="증가" size="sm" onClick={handleIncrement} />
-      <RoundButton label="감소" color="default" onClick={handleDecrement} />
-      <RoundButton label="감소" size="lg" onClick={handleDecrement} />
-      <RoundButton
-        label="감소"
-        color="default"
-        size="xl"
-        onClick={handleDecrement}
-      />
-    </div>
+    <HomePageContainer>
+      <Banner />
+      <BestClassList />
+      <NewClassList />
+      <BestBoardList />
+    </HomePageContainer>
+
+    // <div>
+    //   <button onClick={() => setModalOpen(true)}>모달 오픈</button>
+    //   {modalOpen && (
+    //     <Modal onClose={() => setModalOpen(false)}>
+    //       <Div>hello</Div>
+    //     </Modal>
+    //   )}
+    //   <h1>count: {count}</h1>
+    //   <Button label="증가" size="sm" onClick={handleIncrement} />
+    //   <Button label="증가" size="md" onClick={handleIncrement} />
+    //   <Button
+    //     label="감소"
+    //     color="default"
+    //     size="lg"
+    //     onClick={handleDecrement}
+    //   />
+    //   <Button
+    //     label="감소"
+    //     color="default"
+    //     size="xl"
+    //     onClick={handleDecrement}
+    //   />
+    //   <OutlineButton label="증가" size="sm" onClick={handleIncrement} />
+    //   <OutlineButton label="감소" color="default" onClick={handleDecrement} />
+    //   <OutlineButton
+    //     label="감소"
+    //     size="lg"
+    //     color="ACCENT1"
+    //     onClick={handleDecrement}
+    //   />
+    //   <OutlineButton
+    //     label="감소"
+    //     size="xl"
+    //     color="ACCENT1"
+    //     onClick={handleDecrement}
+    //   />
+    //   <RoundButton label="증가" size="sm" onClick={handleIncrement} />
+    //   <RoundButton label="감소" color="default" onClick={handleDecrement} />
+    //   <RoundButton label="감소" size="lg" onClick={handleDecrement} />
+    //   <RoundButton
+    //     label="감소"
+    //     color="default"
+    //     size="xl"
+    //     onClick={handleDecrement}
+    //   />
+    // </div>
   );
 }
 
