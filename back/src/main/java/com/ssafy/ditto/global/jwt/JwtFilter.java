a34d37c87518647dcb523430a558b3bdb3623f35
이서빈 <bommy00@naver.com>
2024-07-18 11:07:54 +0900

    ✨[feat] #S11P12A106-27 #in-progress JwtFilter에서 JWT 필터링 구현

--- back/src/main/java/com/ssafy/ditto/global/jwt/JwtFilter.java
+++ back/src/main/java/com/ssafy/ditto/global/jwt/JwtFilter.java
@@ -17,6 +17,17 @@ public class JwtFilter extends OncePerRequestFilter {
     public final static String HEADER_NAME="Authorization";
     private final JwtProvider jwtProvider;
 
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String token = resolveToken(request);
+        if (token != null) {
+            jwtProvider.validateToken(token, false);
+            Authentication authentication = this.jwtProvider.getAuthentication(token);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+        filterChain.doFilter(request, response);
+    }
+
     private String resolveToken(HttpServletRequest request) {
         String bearerToken = request.getHeader(HEADER_NAME);
         if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer")) {
