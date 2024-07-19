5335d1c217179ed6af3ce2cbde0d6ca89a594d75
김규태 <wavekt98@gmail.com>
2024-07-19 09:02:20 +0900

    :sparkles:[feat] #S11P12A106-59 #in-progress 게시판 CRUD Controller 구현

--- back/src/main/java/com/ssafy/ditto/domain/post/service/PostService.java
+++ back/src/main/java/com/ssafy/ditto/domain/post/service/PostService.java
@@ -9,14 +9,13 @@ import com.ssafy.ditto.domain.post.dto.PostList;
 public interface PostService {
     String writePost(PostRequest postReq) throws Exception;
     PostList searchPost(Map<String,String> map) throws Exception;
-    PostList bestPost(Map<String,String> map) throws Exception;
-    PostList userPost(Map<String,String> map) throws Exception;
+    PostList bestPost() throws Exception;
+//    PostList userPost(Map<String,String> map) throws Exception;
     Post getPost(int postId) throws Exception;
-    void modifyPost(PostRequest postReq) throws Exception;
-    void deletePost(int postId) throws Exception;
+    String modifyPost(PostRequest postReq) throws Exception;
+    String deletePost(int postId) throws Exception;
 
-    void addView(int postId) throws Exception;
-    void addLike(int postId, int userId) throws Exception;
-    int removeLike(int postId, int userId) throws Exception;
-    int checkLike(int postId, int userId) throws Exception;
+    String addLike(int postId, int userId) throws Exception;
+    String removeLike(int postId, int userId) throws Exception;
+    Boolean checkLike(int postId, int userId) throws Exception;
 }
