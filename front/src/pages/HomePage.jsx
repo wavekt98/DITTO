53b90d9e15ff34417a4302c4988c7e2d9a5da636
sehaim <seha3722@gmail.com>
2024-07-19 10:51:58 +0900

    :art:[Feat] #S11P12A106-54 #in-progress Class List Item 컴포넌트 초기 구조

--- front/src/pages/HomePage.jsx
+++ front/src/pages/HomePage.jsx
@@ -5,7 +5,7 @@ import { styled } from "styled-components";
 // import { increment, decrement } from "../features/counter/counterSlice";
 import Banner from "../components/home/Banner";
 import BestBoardList from "../components/home/BestBoardList";
-import BestClassList from "../components/home/BestClassList";
+import BestClass from "../components/home/BestClass";
 import NewClassList from "../components/home/NewClassList";
 
 const HomePageContainer = styled.div`
@@ -18,57 +18,10 @@ function HomePage() {
   return (
     <HomePageContainer>
       <Banner />
-      <BestClassList />
+      <BestClass />
       <NewClassList />
       <BestBoardList />
     </HomePageContainer>
-
-    // <div>
-    //   <button onClick={() => setModalOpen(true)}>모달 오픈</button>
-    //   {modalOpen && (
-    //     <Modal onClose={() => setModalOpen(false)}>
-    //       <Div>hello</Div>
-    //     </Modal>
-    //   )}
-    //   <h1>count: {count}</h1>
-    //   <Button label="증가" size="sm" onClick={handleIncrement} />
-    //   <Button label="증가" size="md" onClick={handleIncrement} />
-    //   <Button
-    //     label="감소"
-    //     color="default"
-    //     size="lg"
-    //     onClick={handleDecrement}
-    //   />
-    //   <Button
-    //     label="감소"
-    //     color="default"
-    //     size="xl"
-    //     onClick={handleDecrement}
-    //   />
-    //   <OutlineButton label="증가" size="sm" onClick={handleIncrement} />
-    //   <OutlineButton label="감소" color="default" onClick={handleDecrement} />
-    //   <OutlineButton
-    //     label="감소"
-    //     size="lg"
-    //     color="ACCENT1"
-    //     onClick={handleDecrement}
-    //   />
-    //   <OutlineButton
-    //     label="감소"
-    //     size="xl"
-    //     color="ACCENT1"
-    //     onClick={handleDecrement}
-    //   />
-    //   <RoundButton label="증가" size="sm" onClick={handleIncrement} />
-    //   <RoundButton label="감소" color="default" onClick={handleDecrement} />
-    //   <RoundButton label="감소" size="lg" onClick={handleDecrement} />
-    //   <RoundButton
-    //     label="감소"
-    //     color="default"
-    //     size="xl"
-    //     onClick={handleDecrement}
-    //   />
-    // </div>
   );
 }
 
