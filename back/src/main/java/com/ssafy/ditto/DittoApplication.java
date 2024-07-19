f934fc1e46391941c4d036ea4ba713dd89c57551
이서빈 <bommy00@naver.com>
2024-07-19 11:00:43 +0900

    🐛[Feat] #S11P12A106-60 #in-progress BaseTimeEntity 자동 생성 안되는 현상 해결

--- back/src/main/java/com/ssafy/ditto/DittoApplication.java
+++ back/src/main/java/com/ssafy/ditto/DittoApplication.java
@@ -2,8 +2,10 @@ package com.ssafy.ditto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class DittoApplication {
 
     public static void main(String[] args) {
