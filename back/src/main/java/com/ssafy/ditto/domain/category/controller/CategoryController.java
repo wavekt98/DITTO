6268b574a1a51ce08d546899b23251dae3843042
이서빈 <bommy00@naver.com>
2024-07-19 11:08:42 +0900

    ✨[Feat] #S11P12A106-60 #in-progress Category 전체 조회 API 구현

--- back/src/main/java/com/ssafy/ditto/domain/category/controller/CategoryController.java
+++ back/src/main/java/com/ssafy/ditto/domain/category/controller/CategoryController.java
@@ -1,4 +1,24 @@
 package com.ssafy.ditto.domain.category.controller;
 
+import com.ssafy.ditto.domain.category.dto.CategoryResponse;
+import com.ssafy.ditto.domain.category.service.CategoryService;
+import com.ssafy.ditto.global.dto.ResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/categories")
+@RequiredArgsConstructor
 public class CategoryController {
+    private final CategoryService categoryService;
+
+    @GetMapping
+    public ResponseDto<List<CategoryResponse>> getAllCategories() {
+        List<CategoryResponse> categories = categoryService.getAllCategories();
+        return ResponseDto.of(200, "Success", categories);
+    }
 }
