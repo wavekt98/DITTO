693742e0d3e5811e8fed9d83110258ef50043164
이서빈 <bommy00@naver.com>
2024-07-19 13:40:24 +0900

    ✨[Feat] #S11P12A106-60 #in-progress Class 생성 관련 로직 구현

--- back/src/main/java/com/ssafy/ditto/domain/classes/service/ClassService.java
+++ back/src/main/java/com/ssafy/ditto/domain/classes/service/ClassService.java
@@ -1,4 +1,7 @@
 package com.ssafy.ditto.domain.classes.service;
 
+import com.ssafy.ditto.domain.classes.dto.ClassRequest;
+
 public interface ClassService {
+    void createClass(ClassRequest classRequest);
 }
