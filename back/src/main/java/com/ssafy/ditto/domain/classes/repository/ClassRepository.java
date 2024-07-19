64d1419e939fe67655004e9e37257815d1e923d9
이서빈 <bommy00@naver.com>
2024-07-19 11:06:24 +0900

    🐛[Fix] #S11P12A106-60 #in-progress 기본 클래스 사용하지 않도록 변경

--- back/src/main/java/com/ssafy/ditto/domain/classes/repository/ClassRepository.java
+++ back/src/main/java/com/ssafy/ditto/domain/classes/repository/ClassRepository.java
@@ -1,6 +1,7 @@
 package com.ssafy.ditto.domain.classes.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import com.ssafy.ditto.domain.classes.domain.Class;
 
 public interface ClassRepository extends JpaRepository<Class, Integer> {
 }
