04b406472757669a137d940a0355fe925ebb7dac
이서빈 <bommy00@naver.com>
2024-07-19 13:51:19 +0900

    ✨[Feat] #S11P12A106-60 #done Controller 어노테이션 추가

--- back/src/main/java/com/ssafy/ditto/domain/review/controller/ReviewController.java
+++ back/src/main/java/com/ssafy/ditto/domain/review/controller/ReviewController.java
@@ -1,4 +1,11 @@
 package com.ssafy.ditto.domain.review.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/reviews")
+@RequiredArgsConstructor
 public class ReviewController {
 }
