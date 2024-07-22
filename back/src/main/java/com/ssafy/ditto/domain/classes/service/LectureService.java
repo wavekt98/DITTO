4bb490cd0d4de0c97a48e8b89b8666d9c945f4f0
이서빈 <bommy00@naver.com>
2024-07-22 15:53:33 +0900

    ✨[Feat] #S11P12A106-83 #in-progress Lecture 추가 관련 로직 구현

---
+++ back/src/main/java/com/ssafy/ditto/domain/classes/service/LectureService.java
@@ -0,0 +1,10 @@
+package com.ssafy.ditto.domain.classes.service;
+
+import com.ssafy.ditto.domain.classes.dto.LectureRequest;
+import com.ssafy.ditto.domain.classes.dto.LectureResponse;
+
+import java.util.List;
+
+public interface LectureService {
+    void createLecture(Integer classId, LectureRequest lectureRequest);
+}
