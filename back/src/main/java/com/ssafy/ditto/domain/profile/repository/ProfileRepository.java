4de81dea078e7380965b8d6f087367f6a4417941
김규태 <wavekt98@gmail.com>
2024-07-22 17:48:25 +0900

    :sparkles:[Feat] #S11P12A106-107 #done profile 관련 domain dto service repository 생성

---
+++ back/src/main/java/com/ssafy/ditto/domain/profile/repository/ProfileRepository.java
@@ -0,0 +1,42 @@
+package com.ssafy.ditto.domain.profile.repository;
+
+import com.ssafy.ditto.domain.user.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+public interface ProfileRepository extends JpaRepository<User,Integer> {
+
+    @Query("SELECT COUNT(lu) FROM Like_User lu WHERE lu.likeGetterId = :userId")
+    int countLikesByUserId(@Param("userId") int userId);
+
+    @Query("SELECT SUM(c.studentSum) FROM Class c WHERE c.userId = :userId")
+    Integer getTotalStudentSumByUserId(@Param("userId") int userId);
+
+    @Query("SELECT SUM(c.ratingSum) FROM Class c WHERE c.userId = :userId")
+    Integer getTotalRatingSumByUserId(@Param("userId") int userId);
+
+    @Query("SELECT SUM(c.reviewCount) FROM Class c WHERE c.userId = :userId")
+    Integer getTotalReviewCountByUserId(@Param("userId") int userId);
+
+
+    // 좋아요 추가
+    @Modifying
+    @Transactional
+    @Query(value = "INSERT INTO Like_Post (post_id, user_id, created_date) VALUES (:postId, :userId, now())", nativeQuery = true)
+    void addLike(@Param("postId") int postId, @Param("userId") int userId);
+
+    // 좋아요 삭제
+    @Modifying
+    @Transactional
+    @Query(value = "DELETE FROM Like_Post WHERE post_id = :postId AND user_id = :userId", nativeQuery = true)
+    void removeLike(@Param("postId") int postId, @Param("userId") int userId);
+
+    // 좋아요 상태 확인
+    @Query(value = "SELECT COUNT(*) FROM Like_Post WHERE post_id = :postId AND user_id = :userId", nativeQuery = true)
+    int checkLike(@Param("postId") int postId, @Param("userId") int userId);
+}
--- back/src/main/java/com/ssafy/ditto/domain/tag/repository/TagRepository.java
+++ back/src/main/java/com/ssafy/ditto/domain/tag/repository/TagRepository.java
@@ -3,6 +3,10 @@ package com.ssafy.ditto.domain.tag.repository;
 import com.ssafy.ditto.domain.tag.domain.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TagRepository extends JpaRepository<Tag, Integer> {
     Tag findByTagName(String tagName);
+
+    List<Tag> findByUserId(int userId);
 }
