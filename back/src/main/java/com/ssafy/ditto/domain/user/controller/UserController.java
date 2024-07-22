c937d6525a389f48dbf5eda925a194e4de667f76
권재현 <rnjswogus9898@naver.com>
2024-07-22 16:07:24 +0900

    :sparkles:[Feat] #S11P12A106-62 #in-progress 자체 회원 로그인 구현완료. accessToken에 이메일, 닉네임만 있고 프로필사진 Url은 아직 없음

--- back/src/main/java/com/ssafy/ditto/domain/user/controller/UserController.java
+++ back/src/main/java/com/ssafy/ditto/domain/user/controller/UserController.java
@@ -1,9 +1,11 @@
 package com.ssafy.ditto.domain.user.controller;
 
 import com.ssafy.ditto.domain.user.dto.ProSignUpRequest;
+import com.ssafy.ditto.domain.user.dto.UserLoginRequest;
 import com.ssafy.ditto.domain.user.dto.UserSignUpRequest;
 import com.ssafy.ditto.domain.user.service.UserService;
 import com.ssafy.ditto.global.dto.ResponseDto;
+import com.ssafy.ditto.global.jwt.dto.JwtResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -36,4 +38,15 @@ public class UserController {
         return ResponseDto.of(201, "강사 회원 가입 성공");
     }
 
+    //login_001
+    //jwt에 있는 유저 정보 : 이메일, 닉네임, 프로필사진(미구현)
+    @PostMapping("/login")
+    public ResponseDto<JwtResponse> login(@RequestBody UserLoginRequest userLoginRequest){
+        JwtResponse jwtResponse = userService.login(userLoginRequest);
+        if (jwtResponse == null){
+            return ResponseDto.of(400, "로그인 실패", null);
+        } else {
+            return ResponseDto.of(201, "로그인 성공", jwtResponse);
+        }
+    }
 }
--- back/src/main/java/com/ssafy/ditto/global/jwt/dto/JwtResponse.java
+++ back/src/main/java/com/ssafy/ditto/global/jwt/dto/JwtResponse.java
@@ -1,9 +1,11 @@
 package com.ssafy.ditto.global.jwt.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
 @AllArgsConstructor
 public class JwtResponse {
     private final String accessToken;
