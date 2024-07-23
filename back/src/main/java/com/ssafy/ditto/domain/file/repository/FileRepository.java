93a1e8f9121943fe9839d920dc183c6f3ab21669
김규태 <wavekt98@gmail.com>
2024-07-23 13:43:20 +0900

    :sparkles:[Feat] #S11P12A106-75 #in-progress 게시글 파일 구조 일부 수정 및 추가

--- back/src/main/java/com/ssafy/ditto/domain/file/repository/FileRepository.java
+++ back/src/main/java/com/ssafy/ditto/domain/file/repository/FileRepository.java
@@ -2,6 +2,7 @@ package com.ssafy.ditto.domain.file.repository;
 
 import com.ssafy.ditto.domain.file.domain.File;
 
+import com.ssafy.ditto.domain.file.dto.FileResponse;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
