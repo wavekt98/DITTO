81e11769a175658a1a1e3cefe53cdbc34f12bfd1
이서빈 <bommy00@naver.com>
2024-07-24 11:01:41 +0900

    🔧[chore] 태그 추가

--- back/src/main/java/com/ssafy/ditto/domain/answer/controller/AnswerController.java
+++ back/src/main/java/com/ssafy/ditto/domain/answer/controller/AnswerController.java
@@ -1,5 +1,6 @@
 package com.ssafy.ditto.domain.answer.controller;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,5 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/answers")
 @RequiredArgsConstructor
+@Tag(name = "태그")
 public class AnswerController {
 }
