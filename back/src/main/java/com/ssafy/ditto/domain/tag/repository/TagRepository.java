32572e43dc8b02dc3c3c1f00be02bff3a62e20e3
이서빈 <bommy00@naver.com>
2024-07-24 10:44:21 +0900

    fix: repository 어노테이션 추가

--- back/src/main/java/com/ssafy/ditto/domain/tag/repository/TagRepository.java
+++ back/src/main/java/com/ssafy/ditto/domain/tag/repository/TagRepository.java
@@ -2,9 +2,11 @@ package com.ssafy.ditto.domain.tag.repository;
 
 import com.ssafy.ditto.domain.tag.domain.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface TagRepository extends JpaRepository<Tag, Integer> {
     Tag findByTagName(String tagName);
 }
