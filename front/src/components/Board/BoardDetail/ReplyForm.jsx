21ca9404e74893e795b53a9850441191b71da1e8
seoyoung927 <seoyoung0000@naver.com>
2024-07-25 08:07:47 +0900

    :sparkles: [Feat] #S11P12A106-137 #in-progress 게시판 댓글 API 등록

--- front/src/components/Board/BoardDetail/ReplyForm.jsx
+++ front/src/components/Board/BoardDetail/ReplyForm.jsx
@@ -1,9 +1,5 @@
 import { useState } from "react";
 import { styled } from "styled-components";
-import { useSelector } from "react-redux";
-import { useParams } from "react-router-dom";
-
-import useAxios from "../../../hooks/useAxios";
 import OutlineButton from "../../common/OutlineButton";
 import Button from "../../common/Button";
 
@@ -32,25 +28,17 @@ const ButtonWrapper = styled.div`
   gap: 8px;
 `;
 
-function ReplyForm({ isCancel, onCancel, parentId }) {
-  const { sendRequest: postComment } = useAxios();
-  const userId = useSelector(state => state.auth.userId);  // 추가된 부분
+function ReplyForm({ isCancel, onCancel, parentId, onAddComment }) {
   const [content, setContent] = useState("");
-  const { postId } = useParams();
 
   const handleContent = (event) => {
     setContent(event.target.value);
-  }
+  };
 
   const handleAdd = () => {
-    alert("ddd");
-    const postData = {
-      "userId": userId,
-      "content": content,
-      "parentId": parentId
-    }
-    console.log(postData);
-    postComment(`/comments/${postId}`, postData, "post");
+    onAddComment(content, parentId);
+    setContent("");
+    if (isCancel) onCancel();
   };
 
   return (
