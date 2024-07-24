302b708511a288f08b956fc14ad846051428c683
sehaim <seha3722@gmail.com>
2024-07-24 17:25:16 +0900

    :sparkles:[Feat] Class Detail Tab bar 구현 및 리뷰 조회 레이아웃 구현

--- front/src/components/Review/ReviewListItem.jsx
+++ front/src/components/Review/ReviewListItem.jsx
@@ -1,23 +1,39 @@
 import { styled } from "styled-components";
 
+const ClassStepContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  background-color: var(--TERTIARY);
+  padding: 2%;
+  border-radius: 10px;
+  justify-content: space-between;
+  margin: 10px 0;
+`;
+
 const ReviewListItemContainer = styled.div`
   display: flex;
   flex-direction: column;
-  height: 200px;
+  height: 150px;
   width: 100%;
   border-style: solid;
+  border-width: 0.5px;
   border-radius: 10px;
   border-color: var(--BORDER_COLOR);
+  padding: 2%;
+  margin: 10px 0;
 `;
 
 const ReviewDetail = styled.div`
   display: flex;
-`
+`;
 
-function ReviewListItem ({review}) {
+function ReviewListItem({ review }) {
   return (
     <ReviewListItemContainer>
       <ReviewDetail></ReviewDetail>
     </ReviewListItemContainer>
   );
 }
+
+export default ReviewListItem;
