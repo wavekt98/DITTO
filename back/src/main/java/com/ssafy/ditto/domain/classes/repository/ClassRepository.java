90ac40795aca8b09da1119a4449bf185c29dbbcf
이서빈 <bommy00@naver.com>
2024-07-25 15:52:14 +0900

    ✨[Feat] #S11P12A106-86 #in-progress Best Class 조회 쿼리문 작성

--- back/src/main/java/com/ssafy/ditto/domain/classes/repository/ClassRepository.java
+++ back/src/main/java/com/ssafy/ditto/domain/classes/repository/ClassRepository.java
@@ -1,8 +1,18 @@
 package com.ssafy.ditto.domain.classes.repository;
 
 import com.ssafy.ditto.domain.classes.domain.DClass;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Repository
 public interface ClassRepository extends JpaRepository<DClass, Integer>, JpaSpecificationExecutor<DClass> {
+    @Query("SELECT c FROM DClass c JOIN LikeClass lc ON c.classId = lc.classId.classId WHERE lc.createdDate > :oneWeekAgo GROUP BY c ORDER BY COUNT(lc) DESC")
+    List<DClass> findPopularClasses(LocalDateTime oneWeekAgo, Pageable pageable);
+
 }
