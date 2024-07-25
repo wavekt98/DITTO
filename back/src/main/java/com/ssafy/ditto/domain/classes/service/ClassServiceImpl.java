79ad677ec11535b6be01c9392aad0983a3ceb02a
이서빈 <bommy00@naver.com>
2024-07-25 15:59:54 +0900

    ✨[Feat] #S11P12A106-87 #in-progress Weekly New Class 목록 조회 로직 구현

--- back/src/main/java/com/ssafy/ditto/domain/classes/service/ClassServiceImpl.java
+++ back/src/main/java/com/ssafy/ditto/domain/classes/service/ClassServiceImpl.java
@@ -266,4 +266,36 @@ public class ClassServiceImpl implements ClassService {
                 .build()).collect(Collectors.toList());
     }
 
+    @Override
+    @Transactional
+    public List<ClassResponse> getLatestClasses() {
+        LocalDateTime oneWeekAgo = LocalDateTime.now().minusDays(7);
+        Pageable pageable = PageRequest.of(0, 8, Sort.by(Sort.Order.desc("createdDate")));
+        List<DClass> recentClasses = classRepository.findRecentClasses(oneWeekAgo, pageable);
+
+        return recentClasses.stream().map(dClass -> ClassResponse.builder()
+                .classId(dClass.getClassId())
+                .className(dClass.getClassName())
+                .classPrice(dClass.getClassPrice())
+                .classHour(dClass.getClassHour())
+                .classMinute(dClass.getClassMinute())
+                .classExplanation(dClass.getClassExplanation())
+                .classMin(dClass.getClassMin())
+                .classMax(dClass.getClassMax())
+                .studentSum(dClass.getStudentSum())
+                .createdDate(dClass.getCreatedDate())
+                .modifiedDate(dClass.getModifiedDate())
+                .isDeleted(dClass.getIsDeleted())
+                .likeCount(dClass.getLikeCount())
+                .reviewCount(dClass.getReviewCount())
+                .averageRating((float) (dClass.getRatingSum() / (dClass.getReviewCount() == 0 ? 1 : dClass.getReviewCount())))
+                .userNickname(dClass.getUserId().getNickname())
+                .file(dClass.getFileId() != null ? FileResponse.builder()
+                        .fileId(dClass.getFileId().getFileId())
+                        .uploadFileName(dClass.getFileId().getUploadFileName())
+                        .fileUrl(dClass.getFileId().getFileUrl())
+                        .build() : null)
+                .user(UserResponse.of(dClass.getUserId()))
+                .build()).collect(Collectors.toList());
+    }
 }
