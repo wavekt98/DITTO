4e773021636ddcc5362f28e54146cdbb6aa239ac
이서빈 <bommy00@naver.com>
2024-07-25 11:16:44 +0900

    ✨[Feat] #S11P12A106-80 #in-progress Step 추가 로직 구현

---
+++ back/src/main/java/com/ssafy/ditto/domain/classes/service/StepService.java
@@ -0,0 +1,11 @@
+package com.ssafy.ditto.domain.classes.service;
+
+import com.ssafy.ditto.domain.classes.dto.StepRequest;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+
+public interface StepService {
+    void addSteps(Integer classId, List<StepRequest> stepRequests, List<MultipartFile> stepFiles) throws IOException;
+}
