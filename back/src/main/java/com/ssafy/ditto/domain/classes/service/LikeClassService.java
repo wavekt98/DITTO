3a098fc7bc718dc3a6dde464655cfc019d65024b
이서빈 <bommy00@naver.com>
2024-07-25 17:26:12 +0900

    ✨[Feat] #S11P12A106-158 #in-progress Class Like 로직 구현

--- back/src/main/java/com/ssafy/ditto/domain/classes/service/LikeClassService.java
+++ back/src/main/java/com/ssafy/ditto/domain/classes/service/LikeClassService.java
@@ -2,4 +2,6 @@ package com.ssafy.ditto.domain.classes.service;
 
 public interface LikeClassService {
     boolean checkLikeStatus(Integer userId, Integer classId);
+
+    void likeClass(Integer classId, Integer userId);
 }
