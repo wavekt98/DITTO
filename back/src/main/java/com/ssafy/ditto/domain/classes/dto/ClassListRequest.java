3b50daf46052f8ca0e47fed36883d850984d17ab
이서빈 <bommy00@naver.com>
2024-07-25 14:28:58 +0900

    ✨[Feat] #S11P12A106-79 #in-progress Class 목록 조회 관련 Dto 생성

---
+++ back/src/main/java/com/ssafy/ditto/domain/classes/dto/ClassListRequest.java
@@ -0,0 +1,17 @@
+package com.ssafy.ditto.domain.classes.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ClassListRequest {
+    private Integer page;
+    private Integer categoryId;
+    private Integer tagId;
+    private String searchBy;
+    private String keyword;
+    private String sortBy;
+}
