62d28079e7997e3de7cdfc087d2249518681ab4e
권재현 <rnjswogus9898@naver.com>
2024-07-25 14:24:52 +0900

    :sparkles:[Feat] #S11P12A106-129 #done 일반 유저 리뷰 조회 API 개발 완료

--- back/src/main/java/com/ssafy/ditto/domain/review/repository/ReviewRepository.java
+++ back/src/main/java/com/ssafy/ditto/domain/review/repository/ReviewRepository.java
@@ -2,6 +2,17 @@ package com.ssafy.ditto.domain.review.repository;
 
 import com.ssafy.ditto.domain.review.domain.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 public interface ReviewRepository extends JpaRepository<Review, Integer> {
+    @Query(value = "SELECT * " +
+            "FROM Review r " +
+            "WHERE r.user_id = :userId AND r.createdDate < :dateTime AND r.is_deleted = false " +
+            "ORDER BY r.createdDate DESC " +
+            "LIMIT 3", nativeQuery = true)
+    List<Review> getReviews(@Param("userId") int userId, @Param("dateTime") LocalDateTime dateTime);
 }
