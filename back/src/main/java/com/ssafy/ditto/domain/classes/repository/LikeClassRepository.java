79d7b530f329e3206a2b96502773053b63a09ab6
이서빈 <bommy00@naver.com>
2024-07-25 15:52:14 +0900

    ✨[Feat] #S11P12A106-86 #in-progress Best Class 조회 쿼리문 작성

---
+++ back/src/main/java/com/ssafy/ditto/domain/classes/repository/LikeClassRepository.java
@@ -0,0 +1,14 @@
+package com.ssafy.ditto.domain.classes.repository;
+
+import com.ssafy.ditto.domain.classes.domain.DClass;
+import com.ssafy.ditto.domain.classes.domain.LikeClass;
+import com.ssafy.ditto.domain.user.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface LikeClassRepository extends JpaRepository<LikeClass, Integer> {
+    Optional<LikeClass> findByUserIdAndClassId(User userId, DClass classId);
+}
