01b2e8b3ebd78e7c9e905d9f41e9792e7c8826f3
이서빈 <bommy00@naver.com>
2024-07-26 15:20:57 +0900

    🔧[chore] #S11P12A106-161 #in-progress 불필요한 파일 삭제

--- back/src/main/java/com/ssafy/ditto/domain/review/controller/ReviewController.java
+++ back/src/main/java/com/ssafy/ditto/domain/review/controller/ReviewController.java
@@ -1,8 +1,6 @@
 package com.ssafy.ditto.domain.review.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/reviews")
