3ed953c8e1c6d8e03200277b6eda30c969df69c0
이서빈 <bommy00@naver.com>
2024-07-28 22:30:46 +0900

    ♻[Refactor] #S11P12A106-160 #in-progress User userId -> user로 변경

--- back/src/main/java/com/ssafy/ditto/domain/review/domain/Review.java
+++ back/src/main/java/com/ssafy/ditto/domain/review/domain/Review.java
@@ -31,7 +31,7 @@ public class Review extends BaseTimeEntity {
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
-    private User userId;
+    private User user;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "class_id")
