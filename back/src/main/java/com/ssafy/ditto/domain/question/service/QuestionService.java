74b1bd8eea3e7b3837f6c7d45a5a3859d0488352
이서빈 <bommy00@naver.com>
2024-07-28 23:24:48 +0900

    ✨[Feat] #S11P12A106-165 #in-progress 클래스 문의 작성 로직 구현

--- back/src/main/java/com/ssafy/ditto/domain/question/service/QuestionService.java
+++ back/src/main/java/com/ssafy/ditto/domain/question/service/QuestionService.java
@@ -1,4 +1,7 @@
 package com.ssafy.ditto.domain.question.service;
 
+import com.ssafy.ditto.domain.question.dto.QuestionRequest;
+
 public interface QuestionService {
+    void createQuestion(int classId, QuestionRequest questionRequest);
 }
