e425a2d2413dfbff43e82a29f1f91d9f6a13b7a9
이서빈 <bommy00@naver.com>
2024-07-29 00:25:30 +0900

    ✨[Feat] #S11P12A106-168 #done Class Answer 작성 수정 삭제 API 구현

--- back/src/main/java/com/ssafy/ditto/domain/answer/controller/AnswerController.java
+++ back/src/main/java/com/ssafy/ditto/domain/answer/controller/AnswerController.java
@@ -1,13 +1,32 @@
 package com.ssafy.ditto.domain.answer.controller;
 
-import io.swagger.v3.oas.annotations.tags.Tag;
+import com.ssafy.ditto.domain.answer.dto.AnswerRequest;
+import com.ssafy.ditto.domain.answer.service.AnswerService;
+import com.ssafy.ditto.global.dto.ResponseDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/answers")
+@RequestMapping
 @RequiredArgsConstructor
-@Tag(name = "Answer")
 public class AnswerController {
+    private final AnswerService answerService;
+
+    @PostMapping("/questions/{questionId}/answers")
+    public ResponseDto<Void> createAnswer(@PathVariable Integer questionId, @RequestBody AnswerRequest answerRequest) {
+        answerService.createAnswer(questionId, answerRequest);
+        return ResponseDto.of(200, "답변이 성공적으로 작성되었습니다.");
+    }
+
+    @PatchMapping("/answers/{answerId}")
+    public ResponseDto<Void> updateAnswer(@PathVariable Integer answerId, @RequestBody AnswerRequest answerRequest) {
+        answerService.updateAnswer(answerId, answerRequest);
+        return ResponseDto.of(200, "답변이 성공적으로 수정되었습니다.");
+    }
+
+    @DeleteMapping("/answers/{answerId}")
+    public ResponseDto<Void> deleteAnswer(@PathVariable Integer answerId) {
+        answerService.deleteAnswer(answerId);
+        return ResponseDto.of(200, "답변이 성공적으로 삭제되었습니다.");
+    }
 }
