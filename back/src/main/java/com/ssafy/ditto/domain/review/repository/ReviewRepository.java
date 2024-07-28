792915137bdfa154af38c0a35b84d52a6d1ce077
이서빈 <bommy00@naver.com>
2024-07-28 22:29:40 +0900

    ✨[Feat] #S11P12A106-160 #in-progress Class Review 조회 로직 구현

--- back/src/main/java/com/ssafy/ditto/domain/review/repository/ReviewRepository.java
+++ back/src/main/java/com/ssafy/ditto/domain/review/repository/ReviewRepository.java
@@ -1,12 +1,16 @@
 package com.ssafy.ditto.domain.review.repository;
 
+import com.ssafy.ditto.domain.classes.domain.DClass;
 import com.ssafy.ditto.domain.review.domain.Review;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 public interface ReviewRepository extends JpaRepository<Review, Integer> {
     @Query(value = "SELECT * " +
@@ -15,4 +19,6 @@ public interface ReviewRepository extends JpaRepository<Review, Integer> {
             "ORDER BY r.createdDate DESC " +
             "LIMIT 3", nativeQuery = true)
     List<Review> getReviews(@Param("userId") int userId, @Param("dateTime") LocalDateTime dateTime);
+
+    Page<Review> findByClassId(DClass classId, Pageable pageable);
 }
