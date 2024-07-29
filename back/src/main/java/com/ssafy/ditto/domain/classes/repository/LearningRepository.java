d1c303b266795008688f0079e8e18c45b2272fa5
김규태 <wavekt98@gmail.com>
2024-07-29 11:03:01 +0900

    :recycle:[Refactor] #S11P12A106-187 #done 프로필 조회 구조 수정

--- back/src/main/java/com/ssafy/ditto/domain/classes/repository/LearningRepository.java
+++ back/src/main/java/com/ssafy/ditto/domain/classes/repository/LearningRepository.java
@@ -1,2 +1,8 @@
-package com.ssafy.ditto.domain.classes.repository;public class LearningRepository {
+package com.ssafy.ditto.domain.classes.repository;
+
+import com.ssafy.ditto.domain.classes.domain.Learning;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+public interface LearningRepository extends JpaRepository<Learning, Integer>, JpaSpecificationExecutor<Learning> {
 }
