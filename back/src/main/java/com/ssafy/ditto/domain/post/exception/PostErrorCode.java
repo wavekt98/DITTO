0435b498f1f5a1aa571c0d2ffd68bce2ed493189
김규태 <wavekt98@gmail.com>
2024-07-29 13:21:03 +0900

    :recycle:[Refactor] #S11P12A106-186 #done 게시글 좋아요 오류 수정

--- back/src/main/java/com/ssafy/ditto/domain/post/exception/PostErrorCode.java
+++ back/src/main/java/com/ssafy/ditto/domain/post/exception/PostErrorCode.java
@@ -6,7 +6,9 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum PostErrorCode {
-    POST_NOT_EXIST("POST_NOT_EXIST", "게시글이 존재하지 않습니다.");
+    POST_NOT_EXIST("POST_NOT_EXIST", "게시글이 존재하지 않습니다."),
+    POST_LIKE_EXIST_ERROR("POST_LIKE_EXIST_ERROR", "이미 좋아요를 눌렀습니다. 추가할 수 없습니다."),
+    POST_LIKE_CANCEL_ERROR("POST_LIKE_CANCEL_ERROR", "좋아요가 존재하지 않아 취소할 수 없습니다.");
 
     private final String code;
     private final String message;
