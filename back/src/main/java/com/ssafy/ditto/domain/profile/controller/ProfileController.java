9348197a6192e610fbaaaac2e0e22533f31398c8
김규태 <wavekt98@gmail.com>
2024-07-29 10:16:21 +0900

    :sparkles:[Feat] #S11P12A106-110 #done 프로필 내 참여 클래스, 강사의 리뷰리스트 조회 구현

--- back/src/main/java/com/ssafy/ditto/domain/profile/controller/ProfileController.java
+++ back/src/main/java/com/ssafy/ditto/domain/profile/controller/ProfileController.java
@@ -1,13 +1,17 @@
 package com.ssafy.ditto.domain.profile.controller;
 
 import com.ssafy.ditto.domain.classes.domain.DClass;
+import com.ssafy.ditto.domain.classes.dto.ClassListResponse;
 import com.ssafy.ditto.domain.post.dto.PostList;
 import com.ssafy.ditto.domain.profile.dto.ProfileList;
 import com.ssafy.ditto.domain.profile.dto.ProfileResponse;
 import com.ssafy.ditto.domain.profile.service.ProfileService;
 import com.ssafy.ditto.domain.review.domain.Review;
+import com.ssafy.ditto.domain.review.dto.ReviewDetailResponse;
 import com.ssafy.ditto.global.dto.ResponseDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -66,14 +70,17 @@ public class ProfileController {
     }
 
     @GetMapping("/{userId}/class")
-    public ResponseDto<List<DClass>> getUserClass(@PathVariable("userId") int userId){
-
-        return ResponseDto.of(OK.value(), SUCCESS_FETCH.getMessage(),null);
+    public ResponseDto<ClassListResponse> getUserClass(@PathVariable("userId") int userId){
+        ClassListResponse classList = profileService.userClass(userId);
+        return ResponseDto.of(OK.value(), SUCCESS_FETCH.getMessage(),classList);
     }
 
     @GetMapping("/{userId}/review")
-    public ResponseDto<List<Review>> getUserReview(@PathVariable("userId") int userId){
-
+    public ResponseDto<List<Review>> getUserReview(@PathVariable("userId") int userId,
+                                                   @RequestParam int page,
+                                                   @RequestParam int size) {
+        PageRequest pageRequest = PageRequest.of(page, size);
+        Page<ReviewDetailResponse> reviewList = profileService.userReview(userId, pageRequest);
         return ResponseDto.of(OK.value(), SUCCESS_FETCH.getMessage(),null);
     }
 
--- back/src/main/java/com/ssafy/ditto/domain/review/repository/ReviewRepository.java
+++ back/src/main/java/com/ssafy/ditto/domain/review/repository/ReviewRepository.java
@@ -5,6 +5,7 @@ import com.ssafy.ditto.domain.review.domain.Review;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -12,7 +13,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
-public interface ReviewRepository extends JpaRepository<Review, Integer> {
+public interface ReviewRepository extends JpaRepository<Review, Integer> , JpaSpecificationExecutor<Review> {
     @Query(value = "SELECT * " +
             "FROM Review r " +
             "WHERE r.user_id = :userId AND r.createdDate < :dateTime AND r.is_deleted = false " +
