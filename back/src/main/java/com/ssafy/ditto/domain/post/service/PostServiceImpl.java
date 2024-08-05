97763aa94b044555b02dc9d23159b0f14beb5ec2
김규태 <wavekt98@gmail.com>
2024-08-05 10:13:46 +0900

    :bug:[Fix] #S11P12A106-235 #done 검색 수정 완료

--- back/src/main/java/com/ssafy/ditto/domain/post/service/PostServiceImpl.java
+++ back/src/main/java/com/ssafy/ditto/domain/post/service/PostServiceImpl.java
@@ -91,13 +91,23 @@ public class PostServiceImpl implements PostService {
         // searchBy 는 null이 아니라면 "제목", "작성자" 중 하나로 들어옴
         String sb = map.get("searchBy") != null && !map.get("searchBy").isEmpty() ? map.get("searchBy"):null;
         String searchBy = null;
-        if("제목".equals(sb)) searchBy = "title";
-        if("작성자".equals(sb)) searchBy = "nickname";
+        if("제목".equals(sb)) searchBy = "p.title";
+        if("작성자".equals(sb)) searchBy = "u.nickname";
         String keyword = map.get("keyword") != null && !map.get("keyword").isEmpty() ? map.get("keyword"):null;
 
-        List<Post> list = postRepository.getPostLists(boardId, categoryId, tagId, searchBy, keyword);
-        int postCount = postRepository.getPostCount(boardId, categoryId, tagId, searchBy, keyword);
-        int pageCount = (postCount - 1) / sizePage + 1;
+        List<Post> list;
+        int postCount;
+        int pageCount;
+        if(searchBy==null || keyword==null) {
+            list = postRepository.getPostLists(boardId, categoryId, tagId);
+            postCount = postRepository.getPostCount(boardId, categoryId, tagId);
+            pageCount = (postCount - 1) / sizePage + 1;
+        }
+        else{
+            list = postRepository.getPostLists(boardId, categoryId, tagId, searchBy, keyword);
+            postCount = postRepository.getPostCount(boardId, categoryId, tagId, searchBy, keyword);
+            pageCount = (postCount - 1) / sizePage + 1;
+        }
 
         // Sort
         switch (sortBy) {
