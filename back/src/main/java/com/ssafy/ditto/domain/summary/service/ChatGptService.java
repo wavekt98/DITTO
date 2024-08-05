ce66740e3ef808a74fd5b3c9860a74d77153c479
김규태 <wavekt98@gmail.com>
2024-08-05 13:33:32 +0900

    :sparkles:[Feat] #S11P12A106-147 #done Summary API 구현

--- back/src/main/java/com/ssafy/ditto/domain/summary/service/ChatGptService.java
+++ back/src/main/java/com/ssafy/ditto/domain/summary/service/ChatGptService.java
@@ -5,13 +5,15 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class ChatGptService {
     private final OpenAIService openAIService;
 
-    public String summarize(String originText) {
-        return originText.substring(0, originText.length()/2);
+    public String summarize(List<String> originText) {
+        return originText.get(0).substring(0, originText.get(0).length()/2);
 //        return openAIService.summarize(originText);
     }
 }
