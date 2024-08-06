31cd2f5f5e27a50f08a7ce8164f40d9c51ba07fc
이서빈 <bommy00@naver.com>
2024-08-06 13:05:11 +0900

    ✨[Feat] #S11P12A106-236 #in-progress Payment Domain 경로 이동 및 수정

---
+++ back/src/main/java/com/ssafy/ditto/domain/payment/domain/Payment.java
@@ -0,0 +1,63 @@
+package com.ssafy.ditto.domain.payment.domain;
+
+import com.ssafy.ditto.domain.classes.domain.Lecture;
+import com.ssafy.ditto.domain.payment.dto.PayType;
+import com.ssafy.ditto.domain.user.domain.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Table
+public class Payment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Integer paymentId;
+
+    @Column
+    private LocalDateTime payTime;
+
+    @Column
+    private LocalDateTime payCancelTime;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "lecture_id")
+    private Lecture lectureId;
+
+    @Column(nullable = false, name = "pay_type")
+    @Enumerated(EnumType.STRING)
+    private PayType payType;
+
+    @Column(nullable = false, name = "pay_amount")
+    private Long amount;
+
+    @Column(nullable = false, name = "pay_name")
+    private String orderName;
+
+    @Column(nullable = false, name = "order_id")
+    private String orderId;
+
+    private boolean paySuccessYN;
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "user_id")
+    private User userId;
+
+    @Column
+    private String paymentKey;
+
+    @Column
+    private String failReason;
+
+    @Column
+    private boolean cancelYN;
+
+    @Column
+    private String cancelReason;
+}
