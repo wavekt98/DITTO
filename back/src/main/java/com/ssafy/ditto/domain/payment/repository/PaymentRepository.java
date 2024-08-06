934f0c8927175d72cc573747e6a1ad72a2525fc6
이서빈 <bommy00@naver.com>
2024-08-06 15:25:25 +0900

    ✨[Feat] #S11P12A106-236 #in-progress Toss 결제 승인 구현

--- back/src/main/java/com/ssafy/ditto/domain/payment/repository/PaymentRepository.java
+++ back/src/main/java/com/ssafy/ditto/domain/payment/repository/PaymentRepository.java
@@ -2,25 +2,15 @@ package com.ssafy.ditto.domain.payment.repository;
 
 import com.ssafy.ditto.domain.classes.domain.Lecture;
 import com.ssafy.ditto.domain.payment.domain.Payment;
-import com.ssafy.ditto.domain.payment.dto.ChargingHistoryDto;
 import com.ssafy.ditto.domain.user.domain.User;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Optional;
 
 public interface PaymentRepository extends JpaRepository<Payment, Integer> {
-    Optional<Payment> findByOrderId(String orderId);
-
-    Optional<Payment> findByPaymentKeyAndUserId_UserId(String paymentKey, Integer userId);
-
-    Slice<ChargingHistoryDto> findAllByUserId_UserId(Integer userId, Pageable pageable);
-
     // 입력 날짜 이전의 결제 내역 5개 반환
     @Query(value = "SELECT * " +
             "FROM payment p " +
@@ -28,7 +18,6 @@ public interface PaymentRepository extends JpaRepository<Payment, Integer> {
             "ORDER BY p.pay_time DESC " +
             "LIMIT 5", nativeQuery = true)
     List<Payment> getPaymentList(@Param("userId") Integer userId, @Param("dateTime") LocalDateTime dateTime);
-
 
     Payment findByUserIdAndLectureId(User userId, Lecture lectureId);
 }
