18318d8a98783413ed111b6486a1b9c4bbdd49e8
이서빈 <bommy00@naver.com>
2024-08-06 13:05:30 +0900

    ✨[Feat] #S11P12A106-236 #in-progress Payment Repository 경로 이동 및 수정

--- back/src/main/java/com/ssafy/ditto/domain/payment/repository/PaymentRepository.java
+++ back/src/main/java/com/ssafy/ditto/domain/payment/repository/PaymentRepository.java
@@ -1,18 +1,25 @@
-package com.ssafy.ditto.domain.classes.repository;
+package com.ssafy.ditto.domain.payment.repository;
 
 import com.ssafy.ditto.domain.classes.domain.Lecture;
-import com.ssafy.ditto.domain.classes.domain.Payment;
+import com.ssafy.ditto.domain.payment.domain.Payment;
+import com.ssafy.ditto.domain.payment.dto.ChargingHistoryDto;
 import com.ssafy.ditto.domain.user.domain.User;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
-@Repository
 public interface PaymentRepository extends JpaRepository<Payment, Integer> {
+    Optional<Payment> findByOrderId(String orderId);
+
+    Optional<Payment> findByPaymentKeyAndUserId_UserId(String paymentKey, Integer userId);
+
+    Slice<ChargingHistoryDto> findAllByUserId_UserId(Integer userId, Pageable pageable);
 
     // 입력 날짜 이전의 결제 내역 5개 반환
     @Query(value = "SELECT * " +
@@ -20,7 +27,7 @@ public interface PaymentRepository extends JpaRepository<Payment, Integer> {
             "WHERE p.user_id = :userId AND p.pay_time < :dateTime " +
             "ORDER BY p.pay_time DESC " +
             "LIMIT 5", nativeQuery = true)
-    List<Payment> getPaymentList(@Param("userId") int userId, @Param("dateTime") LocalDateTime dateTime);
+    List<Payment> getPaymentList(@Param("userId") Integer userId, @Param("dateTime") LocalDateTime dateTime);
 
 
     Payment findByUserIdAndLectureId(User userId, Lecture lectureId);
