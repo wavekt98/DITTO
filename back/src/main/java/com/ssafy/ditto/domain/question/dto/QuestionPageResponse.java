1d47eaa42ed77ed58b81abdb5fdbc6374be45d38
이서빈 <bommy00@naver.com>
2024-08-07 18:37:00 +0900

    📝[Docs] #S11P12A106-257 #in-progress Question 관련 Swagger 문서화

--- back/src/main/java/com/ssafy/ditto/domain/question/dto/QuestionPageResponse.java
+++ back/src/main/java/com/ssafy/ditto/domain/question/dto/QuestionPageResponse.java
@@ -1,5 +1,6 @@
 package com.ssafy.ditto.domain.question.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
 import java.util.List;
@@ -10,8 +11,14 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 public class QuestionPageResponse {
+
+    @Schema(description = "문의 목록")
     private List<QuestionResponse> questions;
+
+    @Schema(description = "현재 페이지", example = "1")
     private int currentPage;
+
+    @Schema(description = "총 페이지 수", example = "5")
     private int totalPageCount;
 
     public static QuestionPageResponse of(List<QuestionResponse> questions, int currentPage, int totalPageCount) {
