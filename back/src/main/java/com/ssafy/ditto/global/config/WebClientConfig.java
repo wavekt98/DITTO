4a197def64844481ecf2e0c1b5c1af6fe3fe3148
김규태 <wavekt98@gmail.com>
2024-08-07 10:18:14 +0900

    :sparkles:[Feat] #S11P12A106-242 #done chatGPT 연결

--- back/src/main/java/com/ssafy/ditto/global/config/WebClientConfig.java
+++ back/src/main/java/com/ssafy/ditto/global/config/WebClientConfig.java
@@ -3,10 +3,15 @@ package com.ssafy.ditto.global.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class WebClientConfig {
+
+    @Value("${openai.api-key}")
+    private String API_KEY;
+
     @Value("${ai.base.url}")
     public String AI_BASE_URL;
 
@@ -14,6 +19,7 @@ public class WebClientConfig {
     public WebClient webClient() {
         return WebClient.builder()
                         .baseUrl(AI_BASE_URL)
+                        .defaultHeader(HttpHeaders.AUTHORIZATION, "Bearer " + API_KEY)
                         .build();
     }
 }
