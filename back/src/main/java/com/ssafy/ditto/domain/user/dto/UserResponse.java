e30d17baa0565aaa4a2d919655e9808e0d8efa50
이서빈 <bommy00@naver.com>
2024-08-07 18:38:42 +0900

    📝[Docs] #S11P12A106-257 #in-progress User Dto 관련 Swagger 문서화

--- back/src/main/java/com/ssafy/ditto/domain/user/dto/UserResponse.java
+++ back/src/main/java/com/ssafy/ditto/domain/user/dto/UserResponse.java
@@ -1,6 +1,7 @@
 package com.ssafy.ditto.domain.user.dto;
 
 import com.ssafy.ditto.domain.user.domain.User;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
 @Getter
@@ -8,8 +9,13 @@ import lombok.*;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Schema(title = "User Response", description = "사용자 응답")
 public class UserResponse {
+
+    @Schema(description = "사용자 ID", example = "1")
     private Integer userId;
+
+    @Schema(description = "닉네임", example = "SSAFY")
     private String nickname;
 
     public static UserResponse of(User user) {
