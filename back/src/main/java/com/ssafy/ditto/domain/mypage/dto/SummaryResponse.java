01938226adb01c63536bca5039a79e8d15b95166
이서빈 <bommy00@naver.com>
2024-08-08 10:18:20 +0900

    📝[Docs] #S11P12A106-257 #in-progress Mypage Dto 관련 Swagger 문서화

--- back/src/main/java/com/ssafy/ditto/domain/mypage/dto/SummaryResponse.java
+++ back/src/main/java/com/ssafy/ditto/domain/mypage/dto/SummaryResponse.java
@@ -1,12 +1,19 @@
 package com.ssafy.ditto.domain.mypage.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
+@Schema(title = "요약 응답", description = "요약 응답 DTO")
 public class SummaryResponse {
+    @Schema(description = "요약 ID", example = "1")
     private Integer summaryId;
+
+    @Schema(description = "단계 ID", example = "1")
     private Integer stepId;
+
+    @Schema(description = "요약 내용", example = "요약 내용입니다.")
     private String summaryContent;
 }
