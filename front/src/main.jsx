93b30d6a59be7f52db0356cb2f4bf4128b923c72
권재현 <rnjswogus9898@naver.com>
2024-08-08 13:08:16 +0900

    :sparkles:[Feat] 회원가입에 타이머 기능 추가

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -9,9 +9,11 @@ import "./styles/global.css"; // global.css 파일을 import
 import "./styles/color.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </Provider>
+  </React.StrictMode>
 );
