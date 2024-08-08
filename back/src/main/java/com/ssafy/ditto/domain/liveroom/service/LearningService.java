2f64d156082fabd16597328df28be65670b45586
김규태 <wavekt98@gmail.com>
2024-08-08 14:51:03 +0900

    :sparkles:[Feat] #S11P12A106-266 #in-progress 세션 토큰 배치 재생성 불가능하게 구현

--- back/src/main/java/com/ssafy/ditto/domain/liveroom/service/LearningService.java
+++ back/src/main/java/com/ssafy/ditto/domain/liveroom/service/LearningService.java
@@ -3,6 +3,8 @@ package com.ssafy.ditto.domain.liveroom.service;
 import com.ssafy.ditto.domain.liveroom.dto.LearningPageResponse;
 import org.springframework.data.domain.Pageable;
 
+import java.util.List;
+
 public interface LearningService {
     boolean isValidUser(Integer userId, Integer lectureId);
     void changeStatus(Integer lectureId);
@@ -12,4 +14,6 @@ public interface LearningService {
 
     void addStudent(Integer userId, Integer lectureId);
     void deleteStudent(Integer userId, Integer lectureId);
+
+    List<Integer> getStudentList(Integer lectureId);
 }
