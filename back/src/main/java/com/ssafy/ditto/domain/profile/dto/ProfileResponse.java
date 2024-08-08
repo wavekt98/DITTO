e35aa82df0953584348a77507a76ead6ed80c817
이서빈 <bommy00@naver.com>
2024-08-08 10:18:32 +0900

    📝[Docs] #S11P12A106-257 #in-progress Profile Dto 관련 Swagger 문서화

--- back/src/main/java/com/ssafy/ditto/domain/profile/dto/ProfileResponse.java
+++ back/src/main/java/com/ssafy/ditto/domain/profile/dto/ProfileResponse.java
@@ -1,7 +1,7 @@
 package com.ssafy.ditto.domain.profile.dto;
 
-import com.ssafy.ditto.domain.tag.domain.Tag;
 import com.ssafy.ditto.domain.user.domain.User;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -13,23 +13,42 @@ import java.util.List;
 @Setter
 @NoArgsConstructor
 @ToString
+@Schema(title = "프로필 응답", description = "프로필 응답 DTO")
 public class ProfileResponse {
+    @Schema(description = "유저 ID", example = "1")
     private Integer userId;
+
+    @Schema(description = "역할 ID", example = "1")
     private Integer roleId;
+
+    @Schema(description = "닉네임", example = "홍길동")
     private String nickname;
+
+    @Schema(description = "파일 ID", example = "1")
     private Integer fileId;
+
+    @Schema(description = "업로드 파일명", example = "profile.jpg")
     private String uploadFileName;
+
+    @Schema(description = "파일 URL", example = "https://example.com/file")
     private String fileUrl;
+
+    @Schema(description = "좋아요 수", example = "100")
     private Integer likeCount;
+
+    @Schema(description = "학생 수", example = "50")
     private Integer studentSum;
+
+    @Schema(description = "평균 평점", example = "4.5")
     private Float avgRating;
+
+    @Schema(description = "소개", example = "안녕하세요, 홍길동입니다.")
     private String intro;
+
+    @Schema(description = "태그 목록")
     private List<String> tags;
 
-    public ProfileResponse(Integer userId, Integer roleId, String nickname,
-                           Integer fileId, String uploadFileName, String fileUrl,
-                           Integer likeCount, Integer studentSum, Float avgRating,
-                           String intro, List<String> tags) {
+    public ProfileResponse(Integer userId, Integer roleId, String nickname, Integer fileId, String uploadFileName, String fileUrl, Integer likeCount, Integer studentSum, Float avgRating, String intro, List<String> tags) {
         this.userId = userId;
         this.roleId = roleId;
         this.nickname = nickname;
@@ -50,7 +69,6 @@ public class ProfileResponse {
         this.fileUrl = fileUrl;
         this.tags = tags;
     }
-
 
     public static ProfileResponse of(User user, List<String> tags) {
         return new ProfileResponse(
