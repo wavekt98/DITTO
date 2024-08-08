a8c76fc76dbd9a22ed23bd3d7ff44482982d25f9
이서빈 <bommy00@naver.com>
2024-08-08 10:18:20 +0900

    📝[Docs] #S11P12A106-257 #in-progress Mypage Dto 관련 Swagger 문서화

--- back/src/main/java/com/ssafy/ditto/domain/mypage/dto/MilageHistoryResponse.java
+++ back/src/main/java/com/ssafy/ditto/domain/mypage/dto/MilageHistoryResponse.java
@@ -1,5 +1,6 @@
 package com.ssafy.ditto.domain.mypage.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Data;
 
@@ -7,11 +8,23 @@ import java.time.LocalDateTime;
 
 @Data
 @Builder
+@Schema(title = "마일리지 이력 응답", description = "마일리지 이력 응답 DTO")
 public class MilageHistoryResponse {
+    @Schema(description = "이력 ID", example = "1")
     private Integer historyId;
+
+    @Schema(description = "클래스명", example = "클래스 이름입니다.")
     private String className;
+
+    @Schema(description = "마일리지 금액", example = "1000")
     private Integer mileageAmount;
+
+    @Schema(description = "시간", example = "2023-01-01T00:00:00")
     private LocalDateTime time;
+
+    @Schema(description = "상태", example = "1")
     private Integer state;
+
+    @Schema(description = "최종 금액", example = "10000")
     private Integer finalAmount;
 }
