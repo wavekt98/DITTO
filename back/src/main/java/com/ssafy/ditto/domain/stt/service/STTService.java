09aa614cfeb03d8e0e4dedbe2351742552eefede
김규태 <wavekt98@gmail.com>
2024-08-08 11:21:59 +0900

    :recycle:[Refactor] #S11P12A106-253 #done STT 관련 백엔드 삭제

--- back/src/main/java/com/ssafy/ditto/domain/stt/service/STTService.java
+++
@@ -1,9 +0,0 @@
-package com.ssafy.ditto.domain.stt.service;
-
-import org.springframework.web.multipart.MultipartFile;
-
-import java.io.IOException;
-
-public interface STTService {
-    String transcribe(MultipartFile audioFile) throws IOException;
-}
