43554a9de3141bfcd8649a41f6a6efeef042d4a4
김규태 <wavekt98@gmail.com>
2024-08-09 15:22:32 +0900

    :sparkles:[Feat] #S11P12A106-273 #done 마일리지 자동정산 메서드 추가

--- back/src/main/java/com/ssafy/ditto/domain/mypage/repository/MileageHistoryRepository.java
+++ back/src/main/java/com/ssafy/ditto/domain/mypage/repository/MileageHistoryRepository.java
@@ -1,12 +1,15 @@
 package com.ssafy.ditto.domain.mypage.repository;
 
+import com.ssafy.ditto.domain.classes.domain.Lecture;
 import com.ssafy.ditto.domain.mypage.domain.MileageHistory;
+import com.ssafy.ditto.domain.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 public interface MileageHistoryRepository extends JpaRepository<MileageHistory, Integer> {
     @Query(value = "SELECT * " +
@@ -15,4 +18,6 @@ public interface MileageHistoryRepository extends JpaRepository<MileageHistory, 
             "ORDER BY m.time DESC " +
             "LIMIT 10", nativeQuery = true)
     List<MileageHistory> getMileageHistoryList(@Param("userId") int userId, @Param("dateTime") LocalDateTime dateTime);
+
+    Optional<MileageHistory> findByUserAndLecture(User user, Lecture lecture);
 }
