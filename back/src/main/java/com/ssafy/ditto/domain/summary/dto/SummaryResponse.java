ec1667449dc293a5acf5c399f3062a37d86ca3e8
김규태 <wavekt98@gmail.com>
2024-08-13 16:04:26 +0900

    :recycle:[Refactor] #S11P12A106-253 #done SummaryResponse Dto 수정

--- back/src/main/java/com/ssafy/ditto/domain/summary/dto/SummaryResponse.java
+++ back/src/main/java/com/ssafy/ditto/domain/summary/dto/SummaryResponse.java
@@ -9,7 +9,7 @@ import lombok.Data;
 @Schema(title = "요약 응답", description = "요약 응답 DTO")
 public class SummaryResponse {
     @Schema(description = "단계 Id", example = "1")
-    private Integer stepId;
+    private Byte stepNo;
 
     @Schema(description = "단계 정보", example = "굳히기 단계")
     private String stepName;
